db718bc2b37e0269cc44c698f1e7c042dd50d1eb
Bisma Raza <bismauniversity@gmail.com>
2026-07-02 13:40:03 +0500

    Update day_1.js

--- JavaScript - Practice/day_1.js
+++ JavaScript - Practice/day_1.js
@@ -1,1 +1,15 @@
-console.log("Hello, World");
+// Code run on console..
+// Task 1: Age Calculator
+
+let currentyear = 2026;
+let birthyear = 2005;
+
+let age = currentyear - birthyear;
+console.log("Age is :" + age);
+
+// Task 2 : Temprature Converter
+let calcius = 30;
+let fahrenheit = (calcius * 9/5) + 32;
+console.log(calcius + "°C is " + fahrenheit + "°F");
+
+g
