7099cc20f5a24d058e3c589737b8bc5ffc52b5a0
Bisma Raza <bismauniversity@gmail.com>
2026-07-02 14:43:16 +0500

    Update day_2.js

--- JavaScript - Practice/day_2.js
+++ JavaScript - Practice/day_2.js
@@ -1,1 +1,20 @@
 // Task 1 : Grade Calculator
+let numbers = 85;
+
+if (numbers >= 90) {
+    console.log("Grade A");
+}
+else if (numbers >= 80) {
+    console.log("Grade B");
+}
+else if (numbers >= 70) {
+    console.log("Grade C");
+}
+else if (numbers >= 60) {
+    console.log("Grade D");
+}
+else {
+    console.log("Fail");
+}
+
+// Task 2: Login Validation
