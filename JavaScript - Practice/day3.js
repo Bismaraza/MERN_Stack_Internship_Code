4a9a00a1d0818ee1b2da93e280a601a5a6473ac1
Bisma Raza <bismauniversity@gmail.com>
2026-07-06 13:09:48 +0500

    Day 3

--- JavaScript - Practice/day3.js
+++ JavaScript - Practice/day3.js
@@ -1,2 +1,48 @@
 // Arrays
 let students = ["Bisma", "raza", "Anas"];
+
+// Access Arrays
+console.log(students[0]);
+
+// Different types are combined in single array
+
+let className = ["BS", 4, "UMW"];
+
+// Updating
+className[2] = "University";
+'University'
+className[2];
+'University'
+className
+(3) ['BS', 4, 'University']
+
+// Arrays Methods
+// Push: add to end , Pop: Delete from end or return it , Shift: delete from start and return it
+// Unshift: Add to start
+
+className.push("Bisma");
+
+// Objects Literals  : Collection of properties : Property => (Key, Value) Pair 
+
+let student = {
+  name = "Bisma",
+  age = 20,
+};
+
+const student = {
+  name = "Bisma",
+  age = 20,
+};
+
+// Memory address will be same of this object.
+
+// Add update or delete values
+
+student.name = "Raza";
+// New property add 
+student.gender= "Female"; 
+// Delete property
+delete student.gender;
+
+
+// Functions
