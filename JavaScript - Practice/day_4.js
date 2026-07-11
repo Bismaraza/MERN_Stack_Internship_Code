a4d3e3a11be1f88500147859b20015d462cbb84a
Bisma Raza <bismauniversity@gmail.com>
2026-07-11 22:01:07 +0500

    Functions

---
+++ JavaScript - Practice/day_4.js
@@ -0,0 +1,34 @@
+// Modern JS (ES6+)
+  function helloname(){
+    console.log("Hello!");
+  }
+
+//   helloname();
+
+  function table(){
+    for (let index = 1; index <= 5; index++) {
+        console.log(index);
+    }
+  }
+
+//   table();
+
+function isadult(){
+    let age = 20;
+    if(age>=18){
+        console.log("Not Adult");
+    }
+    else
+    {
+        console.log("Adult");
+    }
+}
+
+// isadult();
+
+function poem(){
+    console.log(" Twinkle twinkle Little Star");
+    console.log("How i wonder what you are");
+}
+
+poem();
